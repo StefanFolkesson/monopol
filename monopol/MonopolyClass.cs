1d4bb370a76105485102ce97eebfaa82c57ecfdd
StefanFolkesson <stefan.folkesson@ntig.se>
2024-12-18 16:14:56 +0100

    streets

--- monopol/MonopolyClass.cs
+++ monopol/MonopolyClass.cs
@@ -12,16 +12,10 @@ namespace monopol {
         // public enum card {Chance, CommunityChest};
         public MonopolyClass() {
             List<BoardObject> board = new List<BoardObject>();
-            board.Add(new Street("Hamngatan", 1, "Röd", 3000, 250, 1000));
-            board.Add(new TrainStation("Centralstationen", 5, 2000, 500));
-            board.Add(new ChancePosition("Chansen", 7));
-            board.Add(new Street("Kungsgatan", 9, "Röd", 3000, 250, 1000));
-            board.Add(new Street("Stureplan", 11, "Gul", 3500, 350, 1500));
-            board.Add(new Street("Götgatan", 13, "Gul", 3500, 350, 1500));
-            board.Add(new Street("Odenplan", 15, "Gul", 4000, 400, 2000));
-            board.Add(new TrainStation("Södra Station", 17, 2000, 500));
-            board.Add(new Street("Ringvägen", 19, "Grön", 4500, 450, 2000));
-            board.Add(new Street("Sveavägen", 21, "Grön", 4500, 450, 2000));
+            // Create all the squares on the board and add the swedish names to the streets and add them to the list
+
+
+
 
             gameplayers.Add(new GamePlayer("Spelare 1", 1, 10000));
             gameplayers.Add(new GamePlayer("Spelare 2", 1, 10000));
